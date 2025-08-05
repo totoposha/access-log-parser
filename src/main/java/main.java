75243e240c920f388f954a7353c0ff0782952dbc
JaToT <totopots@mail.ru>
2025-08-05 17:07:35 +0300

    frthcom. remove text lenght calculate. add number calculate

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text= new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstnumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondnumber = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел: "+ (firstnumber+secondnumber));
+        System.out.println("Разница чисел: "+ (firstnumber-secondnumber));
+        System.out.println("Произведение чисел: "+(firstnumber*secondnumber));
+        System.out.println("Частное чисел: "+ ((double)firstnumber/secondnumber));
     }
 }
