aa564599591b730ae0d9d90478850c60b714fe9c
JaToT <totopots@mail.ru>
2025-08-07 08:22:49 +0300

    fvcom. remove number calculate. add file path parser

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,21 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int firstnumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondnumber = new Scanner(System.in).nextInt();
-        System.out.println("Сумма чисел: "+ (firstnumber+secondnumber));
-        System.out.println("Разница чисел: "+ (firstnumber-secondnumber));
-        System.out.println("Произведение чисел: "+(firstnumber*secondnumber));
-        System.out.println("Частное чисел: "+ ((double)firstnumber/secondnumber));
+        int num=1;
+        while(true){
+            System.out.println("Введите путь к файлу:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectiry = file.isDirectory();
+            if (!fileExists||isDirectiry){
+                System.out.println("Файл не существует или указан путь к папке.");
+                continue;
+            }
+            System.out.println("Путь указан верно. Это файл номер "+num);
+            num++;
+        }
     }
 }
